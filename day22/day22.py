988057cef78a6bb90f5870f796cec40293a2dbe8
Jeffrey Nelson <jdn5126@gmail.com>
2017-12-28 19:56:19 -0500

    day 22 part 1

--- day22/day22.py
+++ day22/day22.py
@@ -1,86 +1,66 @@
 # Sporifica Virus
 
+
 import sys
-#from debug import info
-
-# Globals
-# sys.excepthook = info
-grid = []
 
 
-def walk_grid(num_iters):
+def virus_cleaner(grid, num_bursts):
+    center = len(grid) / 2
+    x = center
+    y = center
     num_infected = 0
-    x = len(grid) / 2
-    y = x
-    facing = 0 # 0=UP, 1=RIGHT, 2=DOWN, 3=LEFT
-    for i in range(0, num_iters):
-        # Is the node infected?
-        if grid[x][y] == "#":
-            # Yes: turn to right
+    facing = 0 # 0 - UP, 1 - RIGHT, 2 - DOWN, 3 - LEFT
+    for _ in range(0, num_bursts):
+        # Is current node infected?
+        if grid[y][x] == "#":
+            # Turn to the right
             facing = (facing + 1) % 4
             # Clean node
-            grid[x][y] = "."
+            grid[y][x] = "."
         else:
-            # No: Turn to left
+            # Turn to the left
             if facing == 0:
                 facing = 3
             else:
                 facing -= 1
-            # Infect node, count
-            grid[x][y] = "#"
+            # Infect node
+            grid[y][x] = "#"
             num_infected += 1
-        # Move
-        row_len = len(grid[0])
-        column_len = len(grid)
+        # Move forward one in direction facing
         if facing == 0:
-            # Create new row above?
-            if x == 0:
-                grid.insert(0, ['.']*row_len)
-                x = 0
-            else:
-                x -= 1
-        elif facing == 1:
-            # Create new column to the right?
-            if y == row_len - 1:
-                for row in grid:
-                    row.insert(row_len, ".")
-                y = row_len
-            else:
-                y += 1
-        elif facing == 2:
-            # Create new row below?
-            if x == column_len - 1:
-                grid.insert(column_len, ['.']*row_len)
-            else:
-                x += 1
-        else:
-            # Create new column to the left?
             if y == 0:
-                for row in grid:
-                    row.insert(0, ".")
+                # Insert row above
+                grid.insert(0, list("."*len(grid[0])) )
                 y = 0
             else:
                 y -= 1
+        elif facing == 1:
+            if x == len(grid[y]) - 1:
+                # Add column to the right
+                for row in grid:
+                    row.append(".")
+            x += 1
+        elif facing == 2:
+            if y == len(grid) - 1:
+                # Add row below
+                grid.insert(len(grid), list("."*len(grid[0])) )
+            y += 1
+        elif facing == 3:
+            if x == 0:
+                # Add column to the left
+                for row in grid:
+                    row.insert(0, ".")
+                x = 0
+            else:
+                x -= 1
     return num_infected
 
 
-# Count number of infected nodes in grid
-def count_infected_nodes():
-    infected_nodes = 0
-    for row in grid:
-        infected_nodes += row.count("#")
-    return infected_nodes
-
-
-
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Call as ./day22.py <file> <num_iters>"
+        sys.exit()
     f = open(sys.argv[1], 'r')
-    num_iters = int(sys.argv[2])
-    grid = [ list(x.strip()) for x in f.readlines() ]
-    print "Number of infected nodes at start {0}".format(
-            count_infected_nodes())
-    num_infected = walk_grid(num_iters)
-    print "Number of bursts causing infection: {0}".format(
-            num_infected)
-    print "Number of infected nodes at end {0}".format(
-            count_infected_nodes())
+    grid = [ list(line.strip()) for line in f.readlines() ]
+    num_bursts = int(sys.argv[2])
+    print virus_cleaner(grid, num_bursts)
