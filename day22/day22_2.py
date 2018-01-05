7d80c35d1a8dac50eaf47b772e84ddf4cf7c6ac1
Jeffrey Nelson <jdn5126@gmail.com>
2018-01-04 20:47:11 -0600

    day 22 part 2

---
+++ day22/day22_2.py
@@ -0,0 +1,82 @@
+# Sporifica Virus
+
+
+import sys
+
+
+def virus_cleaner(grid, num_bursts):
+    center = len(grid) / 2
+    x = center
+    y = center
+    num_infected = 0
+    facing = 0 # 0 - UP, 1 - RIGHT, 2 - DOWN, 3 - LEFT
+    for _ in range(0, num_bursts):
+        # Is current node infected?
+        if grid[y][x] == "#":
+            # Infected
+            # Turn to the right
+            facing = (facing + 1) % 4
+            # Flag node
+            grid[y][x] = "F"
+        elif grid[y][x] == ".":
+            # Clean
+            # Turn to the left
+            if facing == 0:
+                facing = 3
+            else:
+                facing -= 1
+            # Weaken node
+            grid[y][x] = "W"
+        elif grid[y][x] == "W":
+            # Weakened
+            # Do not turn
+            # Infect node
+            grid[y][x] = "#"
+            num_infected += 1
+        elif grid[y][x] == "F":
+            # Flagged
+            # Reverse direction
+            facing = (facing + 2) % 4
+            # Clean node
+            grid[y][x] = "."
+        else:
+            print "Node state of %s not recognized" % grid[x][y]
+            sys.exit(1)
+        # Move forward one in direction facing
+        if facing == 0:
+            if y == 0:
+                # Insert row above
+                grid.insert(0, list("."*len(grid[0])) )
+                y = 0
+            else:
+                y -= 1
+        elif facing == 1:
+            if x == len(grid[y]) - 1:
+                # Add column to the right
+                for row in grid:
+                    row.append(".")
+            x += 1
+        elif facing == 2:
+            if y == len(grid) - 1:
+                # Add row below
+                grid.insert(len(grid), list("."*len(grid[0])) )
+            y += 1
+        elif facing == 3:
+            if x == 0:
+                # Add column to the left
+                for row in grid:
+                    row.insert(0, ".")
+                x = 0
+            else:
+                x -= 1
+    return num_infected
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Call as ./day22.py <file> <num_iters>"
+        sys.exit()
+    f = open(sys.argv[1], 'r')
+    grid = [ list(line.strip()) for line in f.readlines() ]
+    num_bursts = int(sys.argv[2])
+    print virus_cleaner(grid, num_bursts)
